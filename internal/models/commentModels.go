d892f593e4c6245c7b5ea44b20a23ac868842828
musishere <mustafabukhari333@gmail.com>
2026-01-20 18:33:18 +0500

    Tag model

--- internal/models/commentModels.go
+++ internal/models/commentModels.go
@@ -1,10 +1,18 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Comments struct {
-	ID       uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
-	PostID   uuid.UUIDs `gorm:"type:uuid;not null" json:"post_id"`
-	UserID   *uuid.UUID `gorm:"type:uuid" json:"user_id"`
-	ParentID uuid.UUID  `gorm:"type:uuid" json:"parent_id"`
+	ID         uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	PostID     uuid.UUIDs `gorm:"type:uuid;not null" json:"post_id"`
+	UserID     *uuid.UUID `gorm:"type:uuid" json:"user_id"`
+	ParentID   uuid.UUID  `gorm:"type:uuid" json:"parent_id"`
+	Content    string     `gorm:"type:text;not null" json:"content"`
+	IsApproved bool       `gorm:"default:true" json:"is_approved"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
 }
